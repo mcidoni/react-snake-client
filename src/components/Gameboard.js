3036ec0cc5c311dc1039f5f8cbb25277edfdcb0b
mcidoni <matthewcidoni@gmail.com>
2020-12-21 11:59:54 -0500

    add snake.js and gameboard.js, additional basic styling

---
+++ src/components/Gameboard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import TailwindCSS from 'tailwindcss'
+
+const GameBoard = () => {
+  return(
+    <div className="gameboard" display="flex">
+      
+    </div>
+  )
+}
+
+export default Gameboard
